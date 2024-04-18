616128ece935b013ebffaf11b4569f5017e7523b
Luiz Silva <l.vilelas@outlook.com>
2024-04-17 21:26:47 -0300

    first commit spring boot initializr

---
+++ chavepix/src/main/java/com/test/itau/chavepix/ChavepixApplication.java
@@ -0,0 +1,16 @@
+package com.test.itau.chavepix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChavepixApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ChavepixApplication.class, args
+
+		);
+	}
+
+}
